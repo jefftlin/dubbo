960cba4fdf600f956a73b9fa997faa1b0716c9c5
laohu <hujie@fuzhi.ai>
2022-09-03 21:41:56 +0800

    fix(*):1. topic加RCR32 2. 超时间时间处理 3. 协议抽离

--- dubbo-rpc/dubbo-rpc-rocketmq/src/main/java/org/apache/dubbo/rpc/rocketmq/RocketMQExporter.java
+++ dubbo-rpc/dubbo-rpc-rocketmq/src/main/java/org/apache/dubbo/rpc/rocketmq/RocketMQExporter.java
@@ -1,26 +1,59 @@
 package org.apache.dubbo.rpc.rocketmq;
 
-import java.util.Map;
+import static org.apache.dubbo.common.constants.CommonConstants.GROUP_KEY;
+import static org.apache.dubbo.common.constants.CommonConstants.INTERFACE_KEY;
+import static org.apache.dubbo.common.constants.CommonConstants.VERSION_KEY;
+import static org.apache.dubbo.common.constants.RegistryConstants.DEFAULT_CATEGORY;
 
+import java.util.Map;
+import java.util.Objects;
+import java.util.zip.CRC32;
+
+import org.apache.dubbo.common.URL;
 import org.apache.dubbo.rpc.Exporter;
 import org.apache.dubbo.rpc.Invoker;
 import org.apache.dubbo.rpc.protocol.AbstractExporter;
 
 public class RocketMQExporter<T> extends AbstractExporter<T> {
 
+	private static final String DEFAULT_PARAM_VALUE = "";
+
+	private static final String NAME_SEPARATOR = "_";
+
 	private final String key;
 
 	private final Map<String, Exporter<?>> exporterMap;
 
-	public RocketMQExporter( Invoker<T> invoker,String key,  Map<String, Exporter<?>> exporterMap) {
+	public RocketMQExporter(Invoker<T> invoker, URL url, Map<String, Exporter<?>> exporterMap) {
 		super(invoker);
-		this.key = key;
+		this.key = toValue(url);
 		this.exporterMap = exporterMap;
 		this.exporterMap.put(key, this);
 	}
 
 	public void afterUnExport() {
 		exporterMap.remove(key, this);
+	}
+	
+	public String getKey() {
+		return this.key;
+	}
+
+	private String toValue(URL url) {
+		String serviceInterface = url.getParameter(INTERFACE_KEY);
+		String version = url.getParameter(VERSION_KEY, DEFAULT_PARAM_VALUE);
+		String group = url.getParameter(GROUP_KEY, DEFAULT_PARAM_VALUE);
+
+		String value = null;
+		if (Objects.equals(url.getParameter("groupModel"), "topic")) {
+			value = DEFAULT_CATEGORY + NAME_SEPARATOR + serviceInterface + NAME_SEPARATOR + version + NAME_SEPARATOR + group;
+		} else {
+			value = DEFAULT_CATEGORY + NAME_SEPARATOR + serviceInterface;
+		}
+		CRC32 crc32 = new CRC32();
+		crc32.update(value.getBytes());
+		value = value.replace(".", "-")+ NAME_SEPARATOR + Long.toString(crc32.getValue());
+		return value;
 	}
 
 }
--- dubbo-rpc/dubbo-rpc-rocketmq/src/main/java/org/apache/dubbo/rpc/rocketmq/RocketMQInvoker.java
+++ dubbo-rpc/dubbo-rpc-rocketmq/src/main/java/org/apache/dubbo/rpc/rocketmq/RocketMQInvoker.java
@@ -1,6 +1,5 @@
 package org.apache.dubbo.rpc.rocketmq;
 
-import static org.apache.dubbo.common.constants.CommonConstants.DEFAULT_TIMEOUT;
 import static org.apache.dubbo.common.constants.CommonConstants.ENABLE_TIMEOUT_COUNTDOWN_KEY;
 import static org.apache.dubbo.common.constants.CommonConstants.PATH_KEY;
 import static org.apache.dubbo.common.constants.CommonConstants.TIMEOUT_ATTACHMENT_KEY;
@@ -42,10 +41,8 @@ import org.apache.rocketmq.remoting.exception.RemotingTooMuchRequestException;
 
 
 public class RocketMQInvoker<T> extends AbstractInvoker<T> {
-
 	
-	
-	private RocketMQCountCodec dubboCountCodec = new RocketMQCountCodec(FrameworkModel.defaultModel());
+	private RocketMQCountCodec rocketMQCountCodec = new RocketMQCountCodec(FrameworkModel.defaultModel());
 	
 	private final ReentrantLock destroyLock = new ReentrantLock();
 
@@ -63,7 +60,7 @@ public class RocketMQInvoker<T> extends AbstractInvoker<T> {
 	
 	private String groupModel;
 	
-	
+	private Integer timeout;
 	
 	public RocketMQInvoker(Class<T> type, URL url,RocketMQProtocolServer rocketMQProtocolServer) {
 		super(type, url);
@@ -72,6 +69,7 @@ public class RocketMQInvoker<T> extends AbstractInvoker<T> {
 		this.groupModel = url.getParameter("groupModel");
 		this.defaultMQProducer = rocketMQProtocolServer.getDefaultMQProducer();
 		this.topic = url.getParameter("topic");
+		this.timeout = url.getParameter(CommonConstants.TIMEOUT_KEY, CommonConstants.DEFAULT_TIMEOUT);
 		Integer queueId = url.getParameter("queueId",Integer.class,-1);
 		if( queueId != -1) {
 			messageQueue = new MessageQueue();
@@ -107,17 +105,18 @@ public class RocketMQInvoker<T> extends AbstractInvoker<T> {
 			// 动态，heap，dirct
 			DynamicChannelBuffer buffer = new DynamicChannelBuffer(2048);
 			
-			dubboCountCodec.encode(channel, buffer, request);
+			rocketMQCountCodec.encode(channel, buffer, request);
 			
 			Message message  = new Message(topic, null, buffer.array());
 			//message.putUserProperty(MessageConst.PROPERTY_MESSAGE_TYPE, "MixAll.REPLY_MESSAGE_FLAG");
 			
-			if(Objects.nonNull(this.groupModel) || Objects.equals(this.groupModel, "select")) {
+			if(!Objects.equals(this.groupModel, "topic")) {
 				message.putUserProperty(CommonConstants.GENERIC_KEY, this.group);
 				message.putUserProperty(CommonConstants.VERSION_KEY, this.version);
 			}
 			message.putUserProperty(RocketMQProtocolConstant.SEND_ADDRESS, NetUtils.getLocalHost());
-			message.putUserProperty(TIMEOUT_KEY, (System.currentTimeMillis()+timeout)+"");
+			Long messageTimeout =  System.currentTimeMillis()+timeout;
+			message.putUserProperty(TIMEOUT_KEY, messageTimeout.toString());
 			message.putUserProperty(RocketMQProtocolConstant.URL_STRING, getUrl().toString());
 			if (isOneway) {
 				if(Objects.isNull(messageQueue)) {
@@ -153,7 +152,7 @@ public class RocketMQInvoker<T> extends AbstractInvoker<T> {
 		Object countdown = RpcContext.getContext().get(TIME_COUNTDOWN_KEY);
 		int timeout = 1000;
 		if (countdown == null) {
-			timeout = (int) RpcUtils.getTimeout(getUrl(), methodName, RpcContext.getContext(), DEFAULT_TIMEOUT);
+			timeout = (int) RpcUtils.getTimeout(getUrl(), methodName, RpcContext.getContext(), this.timeout);
 			if (getUrl().getParameter(ENABLE_TIMEOUT_COUNTDOWN_KEY, false)) {
 				invocation.setObjectAttachment(TIMEOUT_ATTACHMENT_KEY, timeout); // pass timeout to remote server
 			}
@@ -203,7 +202,7 @@ public class RocketMQInvoker<T> extends AbstractInvoker<T> {
 				channel.setUrl(url);
 				
 				HeapChannelBuffer heapChannelBuffer = new HeapChannelBuffer(message.getBody());
-				Object object =(Object)dubboCountCodec.decode(channel, heapChannelBuffer);
+				Object object =(Object)rocketMQCountCodec.decode(channel, heapChannelBuffer);
 				Response response = (Response)object;
 				DefaultFuture.received(channel, response);
 			} catch (Exception e) {
